fff7c6e1beb95be05f63ef9b3940db28672bf6fd
AbhijeetKanawade <kanawadeabhijeet17@gmail.com>
2024-08-26 13:38:36 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+int main(){
+int a,b,sum;
+cout<<"enter a number=";
+cin>>a;
+cout<<"enter another number=";
+cin>>b;
+sum=a+b;
+cout<<"the sum of two number is="<<sum;
+return 0;
+}
